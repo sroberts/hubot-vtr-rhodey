b8402ed2a7968f4b715c722e76c1414b12c21284
Kyle Maxwell <kylem@xwell.org>
2014-02-21 20:21:48 -0600

    Minor PEP8 fixes

--- rhodey.py
+++ rhodey.py
@@ -12,29 +12,34 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-gi = GeoIP.open("data/GeoLiteCity.dat",GeoIP.GEOIP_STANDARD)
+gi = GeoIP.open("data/GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
 
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
   #example redirect
   return redirect(url_for('helloworld'))
 
+
 @app.route("/helloworld")
 def helloworld():
   return "Hello World!"
+
 
 @app.route("/ip/<ip>/geo/maxmind.json")
 def maxmindgeoip(ip):
   gir = gi.record_by_addr(ip)
   return '%s' % json.dumps(gir)
 
+
 @app.route("/ip/<ip>/geo/ipinfo.json")
 def geoip_ipinfo(ip):
   f = urllib2.urlopen('http://ipinfo.io/%s/json' % (ip))
   return '%s' % f.read()
+
 
 @app.route("/ip/<ip>/ipvoid/ipvoid.json")
 def ipvoid(ip):
@@ -51,6 +56,7 @@ def ipvoid(ip):
     else:
         return "Could not find a decision"
     return '%s' % json.dumps(return_list)
+
 
 if __name__ == "__main__":
   #app.debug = True
